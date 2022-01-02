73a7d84b3ab4ad17df5c86e37bac2c6ffb8345b8
aleh-god <aleh.god@gmail.com>
2022-01-02 23:05:48 +0300

    add: List and Details layouts, classes

---
+++ app/src/main/java/by/godevelopment/kroksample/ui/deatails/DetailsViewModel.kt
@@ -0,0 +1,9 @@
+package by.godevelopment.kroksample.ui.deatails
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/by/godevelopment/kroksample/ui/list/ListViewModel.kt
@@ -0,0 +1,9 @@
+package by.godevelopment.kroksample.ui.list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
